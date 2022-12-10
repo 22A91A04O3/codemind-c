c72b7333c7996ab01467b9ee1862a82db42d9130
22A91A04O3 <119720208+22A91A04O3@users.noreply.github.com>
2022-12-10 11:30:33 +0530

    #include<stdio.h> int main() {     int N,i;     scanf("%d",&N);     int a[N];     int max=-9999;     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     if(max<a[i])      {         max=a[i];      }     }     printf("%d",max); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int N,i;
+    scanf("%d",&N);
+    int a[N];
+    int max=-9999;
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    if(max<a[i])
+     {
+        max=a[i];
+     }
+    }
+    printf("%d",max);
+}
