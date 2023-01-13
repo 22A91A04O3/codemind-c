241099a57bb30b37ac14383978cea3bc6c506213
22A91A04O3 <119720208+22A91A04O3@users.noreply.github.com>
2023-01-13 19:24:59 +0530

    #include<stdio.h> int main() {     int n,l;     while(n!=-l)     {         scanf("%d",&n);         if(n==-1)         {             break;         }         printf("%d ",n*n);     } }

---
+++ Infinite.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,l;
+    while(n!=-l)
+    {
+        scanf("%d",&n);
+        if(n==-1)
+        {
+            break;
+        }
+        printf("%d
+",n*n);
+    }
+}
